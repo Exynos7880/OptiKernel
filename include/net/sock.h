3db667f7edeab9dd9dad6760af5d9eb5458a4a89
Eric Dumazet <edumazet@google.com>
2015-03-15 21:12:12 -0700

    net: add sk_fullsock() helper
    [ Upstream commit 1d0ab253872cdd3d8e7913f59c266c7fd01771d0 ]
    
    We have many places where we want to check if a socket is
    not a timewait or request socket. Use a helper to avoid
    hard coding this.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2304,11 +2304,9 @@ static inline struct sock *skb_steal_sock(struct sk_buff *skb)
 
 /* This helper checks if a socket is a full socket,
  * ie _not_ a timewait or request socket.
- * TODO: Check for TCPF_NEW_SYN_RECV when that starts to exist.
- */
 static inline bool sk_fullsock(const struct sock *sk)
 {
-	return (1 << sk->sk_state) & ~(TCPF_TIME_WAIT);
+	return (1 << sk->sk_state) & ~(TCPF_TIME_WAIT | TCPF_NEW_SYN_RECV);
 }
 
 void sock_enable_timestamp(struct sock *sk, int flag);
