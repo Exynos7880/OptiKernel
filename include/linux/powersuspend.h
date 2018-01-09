591c2980266b6d38145efe0e73413444d9556225
Pranav Vashi <neobuddy89@gmail.com>
2018-01-09 04:46:09 +0400

    Asynchronous Fsync from HTC

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -5,6 +5,15 @@
  *
  * Modified by Jean-Pierre Rasquin <yank555.lu@gmail.com>
  *
+ *  v1.1 - make powersuspend not depend on a userspace initiator anymore,
+ *         but use a hook in autosleep instead.
+ *
+ *  v1.2 - make kernel / userspace mode switchable
+ *
+ *  v1.3 - add a hook in display panel driver as alternative kernel trigger
+ *
+ *  v1.4 - add a hybrid-kernel mode, accepting both kernel hooks (first wins)
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -40,9 +49,7 @@ void unregister_power_suspend(struct power_suspend *handler);
 
 void set_power_suspend_state_autosleep_hook(int new_state);
 void set_power_suspend_state_panel_hook(int new_state);
-extern bool power_suspended;
 
 extern bool power_suspended;
 
 #endif
-
