fdd3a335dbcb990825e8249ca4029ba53f514be6
GhaithCraft <GhaithCraft@Gmail.com>
2018-01-12 20:22:09 +0300

    fix compile errors

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 #Cleanup before build
 
 echo "Compile wtih GCC 6.4.1 Toolchain"
-export CROSS_COMPILE=/home/ghaith/android/Toolchains/aarch64-cortex_a53-linux-gnueabi-gcc-6/bin/aarch64-cortex_a53-linux-gnueabi-
+export CROSS_COMPILE=/home/ghaith/android/Toolchains/djb77/aarch64-cortex_a53-linux-gnueabi-gcc-6/bin/aarch64-cortex_a53-linux-gnueabi-
+
 #export CROSS_COMPILE=/home/ghaith/android/Toolchains/gcc-linaro-7.1.1-2017.08-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 ARCH=arm64
 
