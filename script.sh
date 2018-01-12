e7a4af6a6f27a5b8df83b28faad3dd1a2e790271
GhaithCraft <GhaithCraft@Gmail.com>
2018-01-12 12:02:08 +0300

    Update To BQL9

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Cleanup before build
+
+echo "Compile wtih GCC 6.4.1 Toolchain"
+export CROSS_COMPILE=/home/ghaith/android/Toolchains/aarch64-cortex_a53-linux-gnueabi-gcc-6/bin/aarch64-cortex_a53-linux-gnueabi-
+#export CROSS_COMPILE=/home/ghaith/android/Toolchains/gcc-linaro-7.1.1-2017.08-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+ARCH=arm64
+
+echo "CleanUp"
+make clean && make mrproper
+
+echo "Stock DEFCON"
+make Optikernel-a5y17lte_defconfig
+#make Alphabet_Kernel_a5y17lte_defconfig
+make -j3
